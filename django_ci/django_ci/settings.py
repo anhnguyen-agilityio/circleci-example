05f16355b9b731d347a6c80486a9020b567424ab
Anh Nguyen <anh.nguyenkim@asnet.com.vn>
2018-08-16 17:34:28 +0700

    1

--- django_ci/django_ci/settings.py
+++ django_ci/django_ci/settings.py
@@ -74,12 +74,6 @@ WSGI_APPLICATION = 'django_ci.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 DATABASES = {
     'default': {
