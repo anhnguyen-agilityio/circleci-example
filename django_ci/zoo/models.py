49b086e9e7ad40ee6fea77b07670311ebed16a97
Anh Nguyen <anh.nguyenkim@asnet.com.vn>
2018-08-20 09:03:53 +0700

    1

--- django_ci/zoo/models.py
+++ django_ci/zoo/models.py
@@ -8,3 +8,4 @@ from django.db import models
 class Animal(models.Model):
     name = models.CharField(max_length=255)
     sound = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
